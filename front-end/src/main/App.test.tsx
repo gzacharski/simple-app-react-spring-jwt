e196c71a9f45066c9a60d1544f4ee7f15191955b
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-31 11:58:26 +0200

    Add Home, LogIn, SignUp buttons App component.

--- front-end/src/main/App.test.tsx
+++ front-end/src/main/App.test.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("App renders without crashing", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+describe("App contains:", () => {
+  let links: any = null;
+
+  beforeEach(() => {
+    render(<App />);
+    links = screen.getAllByRole("link");
+  });
+
+  test("Home button", () => {
+    const loginBtn = links.filter((link: { innerHTML: string }) =>
+      link.innerHTML.match(/home/i)
+    );
+    expect(loginBtn.length).toBe(1);
+    expect(loginBtn[0]).toBeInTheDocument();
+  });
+
+  test("Login button", () => {
+    const loginBtn = links.filter((link: { innerHTML: string }) =>
+      link.innerHTML.match(/log in/i)
+    );
+    expect(loginBtn.length).toBe(1);
+    expect(loginBtn[0]).toBeInTheDocument();
+  });
+
+  test("Signup button", () => {
+    const loginBtn = links.filter((link: { innerHTML: string }) =>
+      link.innerHTML.match(/sign up/i)
+    );
+    expect(loginBtn.length).toBe(1);
+    expect(loginBtn[0]).toBeInTheDocument();
+  });
 });
--- front-end/src/main/App.tsx
+++ front-end/src/main/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { LoginForm, RegistrationForm, NavButton } from "src/components";
+import { Home } from "src/pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 text-center my-10">
+          <h1>Test App</h1>
+        </div>
+      </div>
+      <div className="row">
+        <Router>
+          <div className="col-2">
+            <NavButton to="/" name="Home" />
+            <NavButton to="/login" name="Log in" />
+            <NavButton to="/sign-up" name="Sign up" />
+          </div>
+          <div className="col-10">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={LoginForm} />
+              <Route path="/sign-up" component={RegistrationForm} />
+              <Redirect to="/" />
+            </Switch>
+          </div>
+        </Router>
+      </div>
     </div>
   );
 }
