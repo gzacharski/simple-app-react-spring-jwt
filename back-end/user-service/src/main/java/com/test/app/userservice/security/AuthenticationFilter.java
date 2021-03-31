4d1ca74f0e0ed2c3c2b7dc20b1a4a7b1cdbaffb0
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-31 22:58:25 +0200

    Add AuthenticationFilter and WebSecurity in userService

---
+++ back-end/user-service/src/main/java/com/test/app/userservice/security/AuthenticationFilter.java
@@ -0,0 +1,87 @@
+package com.test.app.userservice.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.test.app.userservice.dto.UserDTO;
+import com.test.app.userservice.pojo.request.LogInUserRequest;
+import com.test.app.userservice.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private UserService userService;
+    private Environment environment;
+
+    @Autowired
+    public AuthenticationFilter(UserService userService, Environment environment) {
+        this.userService = userService;
+        this.environment = environment;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws AuthenticationException {
+        try {
+            LogInUserRequest credentials = new ObjectMapper()
+                    .readValue(request.getInputStream(), LogInUserRequest.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            credentials.getEmail(),
+                            credentials.getPassword(),
+                            new ArrayList<>()
+                    )
+            );
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain,
+            Authentication authResult
+    ) throws IOException, ServletException {
+        String userEmail = ((User) authResult.getPrincipal()).getUsername();
+        UserDTO userDetails = userService.getUserDetailsByEmail(userEmail);
+
+        String secretToken = environment.getProperty("token.secret");
+
+        String token = Jwts.builder()
+                .setSubject(userDetails.getUserId())
+                .setExpiration(setTokenExpirationTime())
+                .signWith(SignatureAlgorithm.HS256, secretToken)
+                .compact();
+
+        response.addHeader("token", "Bearer " + token);
+        response.addHeader("userId", userDetails.getUserId());
+    }
+
+    private Date setTokenExpirationTime() {
+        long currentTime = System.currentTimeMillis();
+        String expirationTimeStr = environment.getProperty("token.expiration-time");
+        long expirationTime = Long.parseLong(expirationTimeStr);
+
+        return new Date(currentTime + expirationTime);
+    }
+}
---
+++ back-end/user-service/src/main/java/com/test/app/userservice/security/WebSecurity.java
@@ -0,0 +1,57 @@
+package com.test.app.userservice.security;
+
+import com.test.app.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+    private final Environment environment;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public WebSecurity(
+            UserService userService,
+            Environment environment,
+            BCryptPasswordEncoder bCryptPasswordEncoder
+    ) {
+        this.userService = userService;
+        this.environment = environment;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/account/**").permitAll()
+                .and()
+                .addFilter(getAuthenticationFilter());
+
+        http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+
+        AuthenticationFilter authenticationFilter=
+                new AuthenticationFilter(userService,environment);
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
+}
