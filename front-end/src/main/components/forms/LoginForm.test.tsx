c8b1041ba76352925ce3789cfaa55e62d22fd4d4
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-31 16:32:43 +0200

    Change folder structure - bug fix

--- front-end/src/main/components/forms/LoginForm.test.tsx
+++ front-end/src/main/components/forms/LoginForm.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { LoginForm } from "src/components";
+import { LoginForm } from "src/main/components";
 
 test("LoginForm renders without crashing", () => {
   render(<LoginForm />);
