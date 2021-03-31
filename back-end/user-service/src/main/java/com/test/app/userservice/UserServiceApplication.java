f5c80f5a795fd8af44b6748e0ff7a3da08d89452
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-31 20:09:40 +0200

    Enable Eureka client and create bCryptEncoder bean.

--- back-end/user-service/src/main/java/com/test/app/userservice/UserServiceApplication.java
+++ back-end/user-service/src/main/java/com/test/app/userservice/UserServiceApplication.java
@@ -2,7 +2,11 @@ package com.test.app.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
 
@@ -10,4 +14,8 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
+    @Bean
+    public BCryptPasswordEncoder getBCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
