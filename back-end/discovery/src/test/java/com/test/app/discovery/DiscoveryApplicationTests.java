1549dbeef0ead998d19feeb44ad28391326ce0fc
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-31 09:18:19 +0200

    Add discovery service

---
+++ back-end/discovery/src/test/java/com/test/app/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.app.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
