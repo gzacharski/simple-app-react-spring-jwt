98e78f9ca6e1c3ada28322041a4f4599400cc78b
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-31 13:15:05 +0200

    Add loginForm implementation and its tests.

---
+++ front-end/src/components/forms/LoginForm.test.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { LoginForm } from "src/components";
+
+test("LoginForm renders without crashing", () => {
+  render(<LoginForm />);
+});
+
+describe("Login Form has", () => {
+  beforeEach(() => {
+    render(<LoginForm />);
+  });
+
+  test("title", () => {
+    const title = screen.getByRole("heading");
+    expect(title.innerHTML === "Log In").toBeTruthy();
+  });
+
+  describe("fields:", () => {
+    test("username", () => {
+      const username = screen.getByLabelText(/username/i);
+      expect(username).toBeInTheDocument();
+    });
+
+    test("password", () => {
+      const username = screen.getByLabelText(/password/i);
+      expect(username).toBeInTheDocument();
+    });
+  });
+
+  describe("buttons:", () => {
+    test("Log In", () => {
+      const loginBtn = screen.getByRole("button");
+      expect(loginBtn).toBeInTheDocument();
+    });
+
+    test("Log In button has proper name", () => {
+      const loginBtn = screen.getByRole("button");
+      expect(loginBtn.innerHTML.match(/log in/i)).toBeTruthy();
+    });
+  });
+});
+
+describe("Username field changes its value while typing:",()=>{
+    test("field has empty value",()=>{
+        render(<LoginForm/>);
+
+        const username=screen.getByLabelText(/username/i);
+        expect(username.innerHTML.length).toBe(0);
+        expect(username.getAttribute("value")==="").toBeTruthy();
+    })
+
+    test("type test text",async ()=>{
+        render(<LoginForm/>);
+        let username=null;
+        username=await screen.findByLabelText(/username/i);
+        screen.debug(username);
+        
+        userEvent.type(username,"sample text");
+        username=await screen.findByLabelText(/username/i);
+        screen.debug(username);
+
+        expect(username.getAttribute("value")==="sample text").toBeTruthy();
+    })
+})
--- front-end/src/components/forms/LoginForm.tsx
+++ front-end/src/components/forms/LoginForm.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function LoginForm() {
-    return (
-        <div>
-            LoginForm
+  const [username, setUserName] = useState("");
+  const [password, setPassowrd] = useState("");
+
+  return (
+    <div>
+      <h1 className="text-center">Log In</h1>
+      <div>
+        <label htmlFor="login-form-username">Username</label>
+        <div className="input-group my-2">
+          <input
+            id="login-form-username"
+            className="form-control"
+            value={username}
+            type="text"
+            placeholder="Type username..."
+            onChange={(event) => setUserName(event.target.value)}
+          />
         </div>
-    )
+      </div>
+      <div className="input-group my-2">
+        <label htmlFor="login-form-password">Password</label>
+        <div className="input-group">
+          <input
+            id="login-form-password"
+            className="form-control"
+            value={password}
+            type="password"
+            placeholder="Type password..."
+            onChange={(event) => setPassowrd(event.target.value)}
+          />
+        </div>
+      </div>
+      <button
+        className="btn btn-primary"
+        onClick={() => console.log({ username, password })}
+      >
+        Log in
+      </button>
+    </div>
+  );
 }
