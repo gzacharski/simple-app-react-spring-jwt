0b1dd8d983519c754f5082b759b9ca3276c6e444
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-01 10:06:31 +0200

    Enable security in gateway service

---
+++ back-end/gateway/src/main/java/com/test/app/gateway/security/WebSecurity.java
@@ -0,0 +1,35 @@
+package com.test.app.gateway.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private final Environment environment;
+
+    @Autowired
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/user-service/users").permitAll()
+                .antMatchers("/user-service/users/status").authenticated()
+                .and()
+                .addFilter(new AuthenticationFilter(authenticationManager(), environment));
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
